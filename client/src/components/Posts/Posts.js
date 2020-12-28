4352b2c56dfbdb1b3d8aedb840c0f3080a5302eb
ashen780 <ashenm780@gmail.com>
2020-12-28 16:46:41 +0530

    part one finished

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Post from './Post/Post'
 import useStyles from './styles';
- 
+
 function Posts() {
     const classes = useStyles();
+    const posts = useSelector((state)=>state.posts);
+    console.log(posts);
     return (
         <div>
             <h1>Posts</h1>
-            <Post/>
-            <Post/>
+            <Post />
+            <Post />
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-const CONNECTION_URL = "mongodb+srv://social_app:0UuYDnM5pzkldUVT@cluster0.igdct.mongodb.net/WHATSAPP-MERN?retryWrites=true&w=majority";
+app.use('/posts', postRoutes);
+const CONNECTION_URL = "mongodb+srv://social_app:0UuYDnM5pzkldUVT@cluster0.igdct.mongodb.net/memoryDB?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
