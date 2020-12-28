5b727dd583cbaaadd7433184993b542525a7f329
ashen780 <ashenm780@gmail.com>
2020-12-28 14:54:33 +0530

    before lunch implementing redux done

--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
-import express from "express"
-import bodyParser from "body-parse"
-import mongoose from "mongoose"
-import cors from "cors"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://social_app:0UuYDnM5pzkldUVT@cluster0.igdct.mongodb.net/WHATSAPP-MERN?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port : ${PORT}`)))
+    .catch((error) => console.log(error));
+
+
+mongoose.set('useFindAndModify', false);
+
